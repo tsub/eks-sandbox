67326697e3fc19fc6d2e0daafbfd3bd428e002aa
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2021-02-28 07:58:56 +0900

    style: Rename to game-2048

--- terraform/game-2048.tf
+++ terraform/game-2048.tf
@@ -1,19 +1,15 @@
-resource "kubernetes_namespace" "app-2048" {
+resource "kubernetes_namespace" "game-2048" {
   depends_on = [module.eks.cluster_id]
 
   metadata {
-    name = "2048-game"
+    name = "game-2048"
   }
 }
 
-resource "kubernetes_deployment" "app-2048" {
+resource "kubernetes_deployment" "game-2048" {
   metadata {
-    name      = "2048-deployment"
-    namespace = kubernetes_namespace.app-2048.metadata[0].name
-
-    labels = {
-      name = "2048"
-    }
+    name      = "deployment-2048"
+    namespace = kubernetes_namespace.game-2048.metadata[0].name
   }
 
   spec {
@@ -21,14 +17,14 @@ resource "kubernetes_deployment" "app-2048" {
 
     selector {
       match_labels = {
-        name = "2048"
+        "app.kubernetes.io/name" = "app-2048"
       }
     }
 
     template {
       metadata {
         labels = {
-          name = "2048"
+          "app.kubernetes.io/name" = "app-2048"
         }
       }
 
@@ -47,15 +43,15 @@ resource "kubernetes_deployment" "app-2048" {
   }
 }
 
-resource "kubernetes_service" "app-2048" {
+resource "kubernetes_service" "game-2048" {
   metadata {
     name      = "service-2048"
-    namespace = kubernetes_namespace.app-2048.metadata[0].name
+    namespace = kubernetes_namespace.game-2048.metadata[0].name
   }
 
   spec {
     selector = {
-      name = "2048"
+      "app.kubernetes.io/name" = "app-2048"
     }
 
     port {
@@ -65,19 +61,19 @@ resource "kubernetes_service" "app-2048" {
   }
 }
 
-resource "aws_acm_certificate" "app-2048" {
-  domain_name       = "2048.sandbox.tsub.me"
+resource "aws_acm_certificate" "game-2048" {
+  domain_name       = "2048.${local.route53_sandbox_zone}"
   validation_method = "DNS"
 }
 
-resource "aws_acm_certificate_validation" "app-2048" {
-  certificate_arn         = aws_acm_certificate.app-2048.arn
-  validation_record_fqdns = [for record in aws_route53_record.app-2048-validation : record.fqdn]
+resource "aws_acm_certificate_validation" "game-2048" {
+  certificate_arn         = aws_acm_certificate.game-2048.arn
+  validation_record_fqdns = [for record in aws_route53_record.game-2048-validation : record.fqdn]
 }
 
-resource "aws_route53_record" "app-2048-validation" {
+resource "aws_route53_record" "game-2048-validation" {
   for_each = {
-    for dvo in aws_acm_certificate.app-2048.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.game-2048.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
