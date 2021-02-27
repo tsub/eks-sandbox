06c0d814613babc85f820e6a5b871828ca959d7f
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2021-02-27 17:12:40 +0900

    feat: Expand subnets IP address ranges

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+  public_subnets  = ["10.0.96.0/20", "10.0.112.0/20", "10.0.128.0/20"]
 
   enable_nat_gateway = true
 
